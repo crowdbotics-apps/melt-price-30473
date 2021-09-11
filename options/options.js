3ad276bffdb255071cc66f93680dee712bf28cf2
Crowdbotics <team@crowdbotics.com>
2021-09-11 19:11:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "melt_price_30473Identifier",
+  url: "https://melt_price_30473Identifier.botics.co",
+  api: "https://melt_price_30473Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
